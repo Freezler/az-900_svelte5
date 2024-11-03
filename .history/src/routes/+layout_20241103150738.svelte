9d881f949bb9f61f9c91afbae0055b37b0f5f9ac
Freezler <r.l.devries@live.nl>
2024-11-03 15:07:53 +0100

    add: triyng out svelte transitions

---
+++ .history/src/routes/+layout_20241103150738.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+	import { fade, fly } from 'svelte/transition';
+</script>
+
+
+	<div class="bg-gray-900 bg-no-repeat bg-cover bg-center" in:fade={{ duration: 300 }} out:fly={{ duration: 300 }}>
+		{@render children()}
+	</div>
